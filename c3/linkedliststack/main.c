0588780515834cd144fa43b06e979c450b1801f9
Paul Liu <rocliulp@gmail.com>
2012-08-14 22:55:50 +0800

    pending...

--- c3/linkedliststack/main.c
+++ c3/linkedliststack/main.c
@@ -6,32 +6,47 @@ struct node {
   struct node * next;
 };
 
-static struct node * head = 0;
-static struct node * z = 0;
-static struct node * t = 0;
-
-int stackInit () {
+int stackInit (struct node * head) {
+  if (head != 0) {
+    return 1;
+  }
   head = malloc (sizeof (struct node));
-  z = malloc (sizeof (struct node));
-  head -> next = z;
-  head -> key = 0;
-  z -> next = z;
-
+  head -> key = -1;
+  head -> next = 0;
   return 0;
 }
 
-int push (int v) {
-  t = malloc (sizeof (struct node));
+int stackRemove (struct node * head) {
+  if (head == 0) return 1;
+  while (head != 0) {
+    struct node * i = head -> next;
+    free (head);
+    head = i;
+  }
+  return 0;
+}
+
+int push (int v, struct node * head) {
+  if (head == 0) return 1;
+  if (v < 0) return 1;
+  struct node * t = malloc (sizeof (struct node));
   t -> key = v;
+  t -> next = 0;
 
   struct node * i = head -> next;
-  t -> next = i;
   head -> next = t;
+  if (i != 0) {
+    t -> next = i;
+  }
 
   return 0;
 }
 
-int pop (int * v) {
+int pop (int * v, struct node * head) {
+  if (head == 0) return 1;
+  if (v == 0) return 1;
+  if (emptyStack (head)) return 1;
+
   struct node * i = head -> next;
   *v = i -> key;
 
@@ -42,11 +57,40 @@ int pop (int * v) {
   return 0;
 }
 
-int emptyStack () {
-  if (head -> next == z) return 1;
+int emptyStack (struct node * head) {
+  if (head == 0) {
+    return 1;
+  }
+
+  if (head -> next == 0) return 1;
   return 0;
 }
 
 int main (int argc, char* argv[]) {
+  char c = '\0';
+  struct node * head = 0;
+  stackInit (head);
+  while (scanf("%1s", &c) != EOF) {
+    if (c == '(') {
+      printf (" ");
+    } else if (c == '+') {
+      push (c, head);
+    } else if (c == '*') {
+      push (c, head);
+    } else if (c == ')') {
+      while (!emptyStack(head)) {
+        pop (&c, head);
+        printf ("%c", c);
+      }
+    } else if (c > '0' && c < '9') {
+      printf ("%1c", c);
+    } else {
+      printf (" ");
+    }
+  }
+
+  stackRemove (head);
+  printf ("\n\r");
+
   return 0;
 }
