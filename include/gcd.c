ca89cffa13050f7cb2830246655b174e9e523af1
Paul Liu <rocliulp@gmail.com>
2012-08-22 23:05:33 +0800

    finished compile

--- include/gcd.c
+++ include/gcd.c
@@ -1,4 +1,14 @@
 int gcd (int* value, int u, int v) {
+  if (value == 0) return 1;
+  while (u > 0) {
+    if (u < v) {
+      *value = u;
+      u = v;
+      v = *value;
+    }
+    u = u - v;
+  }
+  *value = v;
 
   return 0;
 }
--- include/gcd.h
+++ include/gcd.h
@@ -1,6 +1,6 @@
 #ifndef _GCD_H_
 #define _GCD_H_
 
-int gcd (int* value, int u, int v);
+extern int gcd (int* value, int u, int v);
 
 #endif
