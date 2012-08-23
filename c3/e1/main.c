6d16c4d5e96b5591fdca92abf4e753536a1255b7
Paul Liu <paulliu@outlook.com>
2012-08-23 15:10:36 +0800

    done

--- c3/e1/main.c
+++ c3/e1/main.c
@@ -2,30 +2,45 @@
 
 #include <gcd.h>
 
-#define N 1000
+#define N 10
 
 int main (int argc, char* argv[]) {
-  int array[N][N] = {0};
+  int array[N][N];
   int i = -1;
   int j = -1;
   int value = 0;
   int ret = 0;
-  
+
+  i = -1;
   while (++i < N) {
+    j = -1;
+    while (++j < N) {
+      array[i][j] = 0;
+    }
+  }
+
+  i = 0;
+  while (++i < N) {
+    j = 0;
     while (++j < N) {
       ret = gcd (&value, i, j);
-      if (value == 1) {
-        array[i][j] = 1;
+      if (ret != 0) {
+        printf ("Error: i = %d, j = %d\n", i, j);       
+      } else {
+        if (value == 1) {
+          array[i][j] = 1;
+        }
       }
     }
   }
 
   i = -1;
-  j = -1;
   while (++i < N) {
+    j = -1;
     while (++j < N) {
-      printf ("array[%d][%d] = %d;\t", i, j, array[i, j]);
+      printf ("array[%d][%d]=%d; ", i, j, array[i][j]);
     }
+    printf ("\n\r");
   }
   printf ("\n\r");
 
--- include/gcd.c
+++ include/gcd.c
@@ -1,5 +1,8 @@
 int gcd (int* value, int u, int v) {
   if (value == 0) return 1;
+  if (u < 1) return 1;
+  if (v < 1) return 1;
+
   while (u > 0) {
     if (u < v) {
       *value = u;
