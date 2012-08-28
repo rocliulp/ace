805b18852ef0a564b2f97d002fccc399d4b0f6b5
Paul Liu <rocliulp@gmail.com>
2012-08-28 23:42:45 +0800

    ...

---
+++ c3e10/c3e6/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 9
+
+struct linked_list {
+  int key[N] = {0};
+  int next[N] = {0};
+};
+
+int 
+
+int main (int argc, char* argv[]) {
+  return 0;
+}
