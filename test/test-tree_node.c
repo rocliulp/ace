95088f1d3a7be27535fcc4100ab1a8d1908ae56f
Paul Liu <rocliulp@gmail.com>
2012-09-23 23:39:16 +0800

    test-tree_node.c

---
+++ test/test-tree_node.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <tree_node.h>
+#include <error.h>
+
+int main (int argc, char * [] argv) {
+  char string [1024] = {'\0'};
+  int ret = 0;
+  int i = 0;
+
+  scanf ("%s", string);
+  printf ("%s\n\r", string);
+
+  while (string [i] != '\0') {
+
+  }
+
+  return EC_OK;
+}
