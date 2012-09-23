8a525cebd3dd706890f321c2e10a09d471447d44
Paul Liu <rocliulp@gmail.com>
2012-09-23 16:29:18 +0800

    init tree

---
+++ src/include/tree_node.c
@@ -0,0 +1,5 @@
+struct tree_node {
+  void const * p_void;
+  struct tree_node * p_l;
+  struct tree_node * p_r;
+}
---
+++ src/include/tree_node.h
@@ -0,0 +1,6 @@
+#ifndef _TREE_NODE_H_
+#define _TREE_NODE_H_
+
+struct tree_node;
+
+#endif
