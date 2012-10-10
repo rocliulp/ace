5fb95c76517026939de8494ee9e17273d4bc07ab
Paul Liu <rocliulp@gmail.com>
2012-10-10 11:10:05 +0800

    Removed

--- drawBinTreeInHalf/drawn_tree_node.c
+++ drawBinTreeInHalf/drawn_tree_node.c
@@ -7,4 +7,34 @@ struct drawn_tree_node {
   struct point2d point_info;
 };
 
+int drawn_tree_node_new (struct drawn_tree_node * * pp_tn) {
+  return 0;
+}
 
+int drawn_tree_node_delete (struct drawn_tree_node * p_tn) {
+  return 0;
+}
+
+int drawn_tree_node_GetLeft (struct drawn_tree_node * * pp_tn) {
+  return 0;
+}
+
+int drawn_tree_node_SetLef (struct drawn_tree_node * p_tn) {
+  return 0;
+}
+
+int drawn_tree_node_GetRight (struct drawn_tree_node * * pp_tn) {
+  return 0;
+}
+
+int drawn_tree_node_SetRight (struct drawn_tree_node * p_tn) {
+  return 0;
+}
+
+int drawn_tree_node_GetLocation (struct point * * pp_location) {
+  return 0;
+}
+
+int drawn_tree_node_SetLocation (struct point * p_location) {
+  return 0;
+}
--- drawBinTreeInHalf/drawn_tree_node.h
+++ drawBinTreeInHalf/drawn_tree_node.h
@@ -3,4 +3,16 @@
 
 struct drawn_tree_node;
 
+int drawn_tree_node_new (struct drawn_tree_node * * pp_tn);
+int drawn_tree_node_delete (struct drawn_tree_node * p_tn);
+
+int drawn_tree_node_GetLeft (struct drawn_tree_node * * pp_tn);
+int drawn_tree_node_SetLef (struct drawn_tree_node * p_tn);
+
+int drawn_tree_node_GetRight (struct drawn_tree_node * * pp_tn);
+int drawn_tree_node_SetRight (struct drawn_tree_node * p_tn);
+
+int drawn_tree_node_GetLocation (struct point * * pp_location);
+int drawn_tree_node_SetLocation (struct point * p_location);
+
 #endif
